b6cd3a4ed792b28db963bf5992ed525744bf81db
Tanaka Yo <tanaka@2st.co.jp>
2018-09-30 03:11:50 +0900

    remove post(sendto)

--- sx.js
+++ sx.js
@@ -17,8 +17,7 @@
             console.log('okOK');
         };
         httpRequest.open('GET', 'http://localhost:8001', true);
-        httpRequest.send();
-        console.log('sent');
+        console.log('get');
     };
 
     ext.command_example2 = function (callback) {
